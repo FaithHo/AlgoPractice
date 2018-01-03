66dd809685c8f642d6474d877c4fbe7327344f80
Hui <hui.he0807@gmail.com>
2018-01-02 16:09:55 -0800

    First time to commit.

--- AlgoPractice/Program.cs
+++ AlgoPractice/Program.cs
@@ -10,6 +10,8 @@ namespace AlgoPractice
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            Console.ReadLine();
         }
     }
 }
